8e7717e504386e59f31e0b842b25ba4f9db926fa
Halil_Hasmer <halilpython@gmail.com>
2022-05-20 16:35:49 +0300

    finish chapter 3

--- chapter-3-structure/ch3-challenge.py
+++ chapter-3-structure/ch3-challenge.py
@@ -1,37 +1,32 @@
 # (c) Maximilian Schwarzmüller / Academind GmbH
 
-# *********
-# Imports
-# *********
 from os import path, makedirs
 from pathlib import Path
 
-# *********
-# Main
-# *********
-# A class which allows us to create DiskStorage instances
-
 
 class DiskStorage:
+    """ A class which allows us to create DiskStorage instances """
     def __init__(self, directory_name):
         self.storage_directory = directory_name
 
     def get_directory_path(self):
         return Path(self.storage_directory)
 
-    # This must be called before a file is inserted
     def create_directory(self):
         if (not path.exists(self.get_directory_path())):
             makedirs(self.storage_directory)
 
     # Warning: Directory must exist in advance
     def insert_file(self, file_name, content):
-        file = open(self.get_directory_path() / file_name, 'w')
+        file_path = self.get_directory_path()
+        file = open(file_path / file_name, 'w')
         file.write(content)
         file.close()
-        # Todo: Add proper error handling
+        # TODO: Add proper error handling
 
 
 log_storage = DiskStorage('logs')
 
+# log_storage.create_directory()
+# if we commen-out th line above, the code will work properly
 log_storage.insert_file('test.txt', 'Test')
